563c613b10e033e056309e51c1c72e39a74bb1ff
unkn0wn107 <unknown107@proton.me>
2022-11-10 00:52:17 +0100

    Protect memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 13:45:20 by agaley            #+#    #+#             */
-/*   Updated: 2022/11/09 14:05:14 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/11/10 00:51:59 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*dest2;
 
 	dest2 = (unsigned char *)dest;
+	if (!(dest && src))
+		return ((void *)0);
 	while (n--)
 		dest2[n] = ((unsigned char *)src)[n];
 	return (dest);
