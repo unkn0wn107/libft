708be91861d5028ac394fb298c47cc958b345a8f
ukn0wn107 <unkn0wn@proton.me>
2022-11-10 12:45:28 +0100

    Fix calloc size allocation

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,20 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 10:37:25 by agaley            #+#    #+#             */
-/*   Updated: 2022/11/10 12:37:16 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/11/10 12:44:30 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*mem;
 
-	mem = malloc(count, size);
+	mem = (void *)malloc(count * size);
 	if (!mem)
 		return ((void *)0);
-	ft_bzero(mem, size);
+	ft_bzero(mem, count * size);
 	return (mem);
 }
