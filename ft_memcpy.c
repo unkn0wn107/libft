b8acd69b493e302e8ea0ec8c607a697a741cb0f7
unkn0wn107 <unknown107@proton.me>
2022-11-13 22:36:48 +0100

    memcpy pass all tests

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 13:45:20 by agaley            #+#    #+#             */
-/*   Updated: 2022/11/10 00:51:59 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/11/13 22:34:19 by alex             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*dest2;
 
 	dest2 = (unsigned char *)dest;
-	if (!(dest && src))
+	if (!(dest || src))
 		return ((void *)0);
 	while (n--)
 		dest2[n] = ((unsigned char *)src)[n];
