9d5f2d149ad053e839b62c2eb869f1838b1d97ca
unkn0wn107 <unknown107@proton.me>
2022-11-14 00:14:49 +0100

    Refactor memchr and pass all tests

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 09:30:27 by agaley            #+#    #+#             */
-/*   Updated: 2022/11/10 10:04:46 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/11/14 00:07:50 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,9 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		if (((int *)s)[i] == c)
-			return (&((int *)s)[i]);
-		i++;
-	}
+	s--;
+	while (n-- && s++)
+		if (*((unsigned char *)s) == (unsigned char)c)
+			return ((void *)s);
 	return ((void *)0);
 }
