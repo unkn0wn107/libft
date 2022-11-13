933441b07549b4729e52554410fb3d33c280da78
unkn0wn107 <unknown107@proton.me>
2022-11-13 23:49:18 +0100

    memmove pass all tests

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 13:45:20 by agaley            #+#    #+#             */
-/*   Updated: 2022/11/10 01:45:09 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/11/13 23:47:02 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,23 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	m;
+	void	*tmp;
 
+	if (!(dest || src))
+		return (NULL);
+	tmp = dest;
 	m = n;
-	if (dest && src && (const char *)dest < (const char *)src)
+	if ((const char *)dest > (const char *)src)
 	{
+		dest = dest + n - 1;
+		src = src + n - 1;
 		while (m--)
 			*((unsigned char *)(dest--)) = *((unsigned char *)(src--));
 	}
-	else if (dest && src)
+	else
 	{
 		while (m--)
 			*((unsigned char *)dest++) = *((unsigned char *)src++);
-		dest = dest - n;
 	}
-	return (dest);
+	return (tmp);
 }
