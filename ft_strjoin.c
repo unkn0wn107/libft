99abcd936dde6aa39a0df36cac1087fe0760ac56
unkn0wn107 <unknown107@proton.me>
2022-11-19 22:27:51 +0100

    strjoin pass all tests

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 22:25:49 by agaley            #+#    #+#             */
-/*   Updated: 2022/11/14 22:55:36 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/11/19 22:27:06 by alex             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,21 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	char	*out;
 	char	*str;
 	size_t	len1;
 	size_t	len2;
 
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
-	str = (char *)malloc(len1 + len2 + 1);
+	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	if (!str)
 		return (NULL);
+	out = str;
 	while (len1--)
-		*(str++) = *(s1++);
+		*(str++) = (char)*(s1++);
 	while (len2--)
-		*(str++) = *(s2++);
+		*(str++) = (char)*(s2++);
 	*str = '\0';
-	return (str);
+	return (out);
 }
