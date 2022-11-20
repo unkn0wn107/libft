22b5dcf5f59d3137fbd9833e4a701879d833cbf4
agaley <agaley@student.42lyon.fr>
2022-11-20 22:19:20 +0100

    Fix strtrim - 1 eror remaining

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,34 +6,44 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 22:47:16 by alex              #+#    #+#             */
-/*   Updated: 2022/11/20 04:24:55 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/11/20 22:18:31 by alex             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
+char	ft_issep(char c, char const *charset)
+{
+	size_t	i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
 	size_t	strlen;
-	size_t	i;
 
-	while (s1 && ft_strnstr(s1, set, 1))
-	{
-		i = 0;
-		while (ft_strchr(s1, set[i++]))
-			s1++;
-	}
+	if (!s1)
+		return (NULL);
+	while (*s1 && set && ft_issep(*s1, set))
+		s1++;
 	strlen = ft_strlen(s1);
 	s1 = s1 + strlen;
-	while (ft_strnstr(s1--, set, 1))
-		strlen--;
+	while (set && ft_issep(*(s1 - 1), set) && strlen--)
+		s1--;
 	str = (char *)malloc(sizeof(char) * (strlen + 1));
 	if (!str)
 		return (NULL);
 	s1 = s1 - strlen;
-	ft_strlcpy(str, s1, strlen);
-	str[strlen] = '\0';
+	ft_strlcpy(str, s1, strlen + 1);
 	return (str);
 }
