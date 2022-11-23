b0e132bd91ca92a5cfd1329e50af415d6d69fe96
agaley <agaley@student.42lyon.fr>
2022-11-23 21:42:16 +0100

    Protects calloc size_t overflow

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,21 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 10:37:25 by agaley            #+#    #+#             */
-/*   Updated: 2022/11/23 21:11:28 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/11/23 21:41:23 by alex             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*mem;
 
-	mem = (void *)malloc(count * size);
+	if (size && nmemb > SIZE_MAX / size)
+		return ((void *)0);
+	mem = malloc(nmemb * size);
 	if (!mem)
 		return ((void *)0);
-	ft_bzero(mem, count * size);
+	ft_bzero(mem, nmemb * size);
 	return (mem);
 }
