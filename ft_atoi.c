550ccd8399216a2a518c4be523aa8d84e50ca220
agaley <agaley@student.42lyon.fr>
2022-11-23 03:44:59 +0100

    Reproduce atoi overflow behaviour

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,29 +6,33 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 10:55:04 by agaley            #+#    #+#             */
-/*   Updated: 2022/11/10 10:15:43 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/11/23 03:43:52 by alex             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_atoi(const char *str)
 {
-	int	sign;
-	int	i;
-	int	result;
+		int						sign;
+		size_t					i;
+		unsigned long int	result;
 
-	result = 0;
-	sign = 1;
-	i = 0;
-	while (str[i] == ' ' || (9 <= str[i] && str[i] <= 13))
-		i++;
-	if (str[i] == '-')
-		sign = -1;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while (str[i] && '0' <= str[i] && str[i] <= '9')
-	{
-		result = 10 * result + str[i] - '0';
-		i++;
-	}
-	return (sign * result);
+		result = 0;
+		sign = 1;
+		i = 0;
+		while (str[i] == ' ' || (9 <= str[i] && str[i] <= 13))
+				i++;
+		if (str[i] == '-')
+				sign = -1;
+		if (str[i] == '-' || str[i] == '+')
+				i++;
+		while ('0' <= str[i] && str[i] <= '9')
+		{
+				if (result != (10 * result + (str[i] - '0')) / 10)
+						return ((sign + 1) / -2);
+				result = 10 * result + (str[i] - '0');
+				i++;
+		}
+		return ((int)(sign * result));
 }
