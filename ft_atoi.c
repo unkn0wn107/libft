9e7967450bfefa07101aa0782df8095d162b5ec8
agaley <agaley@student.42lyon.fr>
2022-11-23 04:09:27 +0100

    Norm atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 10:55:04 by agaley            #+#    #+#             */
-/*   Updated: 2022/11/23 03:43:52 by alex             ###   ########lyon.fr   */
+/*   Updated: 2022/11/23 04:08:02 by alex             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 int	ft_atoi(const char *str)
 {
-		int						sign;
-		size_t					i;
-		unsigned long int	result;
+	int					sign;
+	size_t				i;
+	unsigned long int	result;
 
-		result = 0;
-		sign = 1;
-		i = 0;
-		while (str[i] == ' ' || (9 <= str[i] && str[i] <= 13))
-				i++;
-		if (str[i] == '-')
-				sign = -1;
-		if (str[i] == '-' || str[i] == '+')
-				i++;
-		while ('0' <= str[i] && str[i] <= '9')
-		{
-				if (result != (10 * result + (str[i] - '0')) / 10)
-						return ((sign + 1) / -2);
-				result = 10 * result + (str[i] - '0');
-				i++;
-		}
-		return ((int)(sign * result));
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || (9 <= str[i] && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		if (result != (10 * result + (str[i] - '0')) / 10)
+			return ((sign + 1) / -2);
+		result = 10 * result + (str[i] - '0');
+		i++;
+	}
+	return ((int)(sign * result));
 }
