79699bc9dfacaf05dc55a5c51e080aa7ef4c3cf2
ukn0wn107 <unkn0wn@proton.me>
2022-12-08 07:39:25 +0100

    Fix strtrim overflow

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 22:47:16 by alex              #+#    #+#             */
-/*   Updated: 2022/12/01 00:43:42 by alex             ###   ########lyon.fr   */
+/*   Updated: 2022/12/08 07:38:24 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	ft_issep(char c, char const *charset)
 	size_t	i;
 
 	i = 0;
-	while (charset[i])
+	while (charset && charset[i])
 	{
 		if (c == charset[i])
 			return (1);
@@ -33,11 +33,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (!s1)
 		return (NULL);
-	while (*s1 && set && ft_issep(*s1, set))
+	while (*s1 && ft_issep(*s1, set))
 		s1++;
 	strlen = ft_strlen(s1);
 	s1 = s1 + strlen;
-	while (set && ft_issep(*(s1 - 1), set) && strlen != 0 && strlen--)
+	while (strlen != 0 && ft_issep(*(s1 - 1), set) && strlen--)
 		s1--;
 	str = (char *)malloc(sizeof(char) * (strlen + 1));
 	if (!str)
