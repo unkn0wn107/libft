087cddc91386fdf4bd5ff9addefbf67adb11177e
ukn0wn107 <unkn0wn@proton.me>
2022-12-08 07:09:53 +0100

    Fix strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 16:05:44 by agaley            #+#    #+#             */
-/*   Updated: 2022/12/04 23:21:15 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/12/08 07:09:12 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,7 @@ char	*ft_strdup(const char *s1)
 	if (!dup)
 		return ((char *)0);
 	dup[i] = '\0';
-	while (i >= 0)
-	{
+	while (i--)
 		dup[i] = s1[i];
-		i--;
-	}
 	return (dup);
 }
