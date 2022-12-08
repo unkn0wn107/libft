2252bfac84ec64dc209d905a20a452df42945769
ukn0wn107 <unkn0wn@proton.me>
2022-12-08 07:52:34 +0100

    Simplify strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 22:25:49 by agaley            #+#    #+#             */
-/*   Updated: 2022/11/23 21:12:37 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/12/08 07:45:07 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len1;
 	size_t	len2;
 
+	len1 = 0;
 	if (s1)
 		len1 = ft_strlen(s1);
-	else
-		len1 = 0;
+	len2 = 0;
 	if (s2)
 		len2 = ft_strlen(s2);
-	else
-		len2 = 0;
 	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	if (!str)
 		return (NULL);
