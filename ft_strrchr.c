828b962a01235b7506efe13f7c6dc40700cbe093
ukn0wn107 <unkn0wn@proton.me>
2022-12-08 07:26:18 +0100

    Fix strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,26 +6,25 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 13:08:49 by agaley            #+#    #+#             */
-/*   Updated: 2022/12/04 23:19:48 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/12/08 07:25:04 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-	size_t i;
-	char *s2;
+	size_t	len;
+	char	*s2;
 
 	s2 = (char *)s;
-	i = ft_strlen(s2) - 1;
+	len = ft_strlen(s2);
 	if ((char)c == '\0')
-		return ((char *)s + i + 1);
-	while (i >= 0)
-	{
-		if (s2[i] == (char)c)
-			return (&s2[i]);
-		i--;
-	}
+		return ((char *)s + len);
+	if (len == 0)
+		return ((void *)0);
+	while (len--)
+		if (s2[len] == (char)c)
+			return (&s2[len]);
 	return ((void *)0);
 }
