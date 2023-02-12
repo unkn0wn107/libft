7abd4e0b036d6526779d32bfba7f40525ceef38b
agaley <agaley@student.42lyon.fr>
2023-02-12 23:00:55 +0100

    Fix put hex

--- ft_puthex_fd.c
+++ ft_puthex_fd.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 15:30:22 by agaley            #+#    #+#             */
-/*   Updated: 2023/02/12 17:50:38 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2023/02/12 22:07:40 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,14 @@ int	ft_puthex_fd(unsigned long int hex, char low, int fd)
 	int					size;
 
 	if (ft_putstr_fd("0x", fd) == 2)
-		size += 2;
+		size = 2;
 	else
 		return (-1);
 	if (low == 1)
-		size = ft_putunbr_base_fd(hex, "0123456789abcdef", fd);
+		size += ft_putunbr_base_fd(hex, "0123456789abcdef", fd);
 	else if (low == 0)
-		size = ft_putunbr_base_fd(hex, "0123456789ABCDEF", fd);
+		size += ft_putunbr_base_fd(hex, "0123456789ABCDEF", fd);
 	else
 		return (-1);
 	return (size);
 }
-
